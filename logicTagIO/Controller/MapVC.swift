f9f71d8669cbd5376e234ae317e0d190ab4c71b6
dfsf dam <logicappsource@gmail.com>
2017-07-31 15:54:55 +0200

    Requesting User Location, Center Map.

--- logicTagIO/Controller/MapVC.swift
+++ logicTagIO/Controller/MapVC.swift
@@ -8,9 +8,14 @@
 
 import UIKit
 import MapKit
+import CoreLocation
+
 
 class MapVC: UIViewController {
     
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000 //Meteres
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -18,6 +23,8 @@ class MapVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
         
     }
 
@@ -26,11 +33,36 @@ class MapVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBOutlet weak var centerMapBtn: UIButton!
+    @IBAction func centerMapBtn(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+    }
+    
     
 }
+
 
 extension MapVC: MKMapViewDelegate {
-    
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate  else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2 , regionRadius * 2)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) { //User location changed
+        centerMapOnUserLocation()
+    }
+    
+    
+}
