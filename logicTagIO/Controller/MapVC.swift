3e865c6b6b3b4e926505ebd84476ef93fca02dc3
dfsf dam <logicappsource@gmail.com>
2017-08-02 11:01:58 +0200

    Adding 3D Touch peek to collection view

--- logicTagIO/Controller/MapVC.swift
+++ logicTagIO/Controller/MapVC.swift
@@ -46,6 +46,8 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         collectionView?.dataSource = self
         collectionView?.backgroundColor = UIColor.white
         
+        registerForPreviewing(with: self, sourceView: collectionView!)
+        
         pullUpView.addSubview(collectionView!)
         
     }
@@ -284,6 +286,25 @@ extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource{
         popVC.initData(forImage: imageArray[indexPath.row])
         present(popVC, animated: true , completion: nil)
     }
+    
+}
+
+
+extension MapVC: UIViewControllerPreviewingDelegate {
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        guard let indexPath = collectionView?.indexPathForItem(at: location), let cell = collectionView?.cellForItem(at: indexPath) else {return nil}
+        guard let popVC = storyboard?.instantiateViewController(withIdentifier: "PopVC") as? PopVC else {return nil}
+        
+        popVC.initData(forImage: imageArray[indexPath.row])
+        previewingContext.sourceRect = cell.contentView.frame
+        return popVC
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        show(viewControllerToCommit, sender: self)
+    }
+    
     
 }
 
